bf3cf539b577524a30620d3d48b1b3bb5c3f9afa
Tachibana446 <stinger33z@gmail.com>
2016-04-15 17:21:06 +0900

    できた

---
+++ AutoSaveMan/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace AutoSaveMan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.reloadButton = new System.Windows.Forms.Button();
+            this.processComboBox = new System.Windows.Forms.ComboBox();
+            this.onOffButton = new System.Windows.Forms.Button();
+            this.timeTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.countDownFlowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.minLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.secLabel = new System.Windows.Forms.Label();
+            this.CountDownTimer = new System.Windows.Forms.Timer(this.components);
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.countDownCheckBox = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.countDownFlowLayoutPanel.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // reloadButton
+            // 
+            this.reloadButton.Location = new System.Drawing.Point(12, 12);
+            this.reloadButton.Name = "reloadButton";
+            this.reloadButton.Size = new System.Drawing.Size(75, 23);
+            this.reloadButton.TabIndex = 0;
+            this.reloadButton.Text = "更新";
+            this.reloadButton.UseVisualStyleBackColor = true;
+            this.reloadButton.Click += new System.EventHandler(this.reloadButton_Click);
+            // 
+            // processComboBox
+            // 
+            this.processComboBox.FormattingEnabled = true;
+            this.processComboBox.Location = new System.Drawing.Point(12, 41);
+            this.processComboBox.Name = "processComboBox";
+            this.processComboBox.Size = new System.Drawing.Size(506, 23);
+            this.processComboBox.TabIndex = 1;
+            // 
+            // onOffButton
+            // 
+            this.onOffButton.Location = new System.Drawing.Point(12, 111);
+            this.onOffButton.Name = "onOffButton";
+            this.onOffButton.Size = new System.Drawing.Size(75, 23);
+            this.onOffButton.TabIndex = 2;
+            this.onOffButton.Text = "開始";
+            this.onOffButton.UseVisualStyleBackColor = true;
+            this.onOffButton.Click += new System.EventHandler(this.onOffButton_Click);
+            // 
+            // timeTextBox
+            // 
+            this.timeTextBox.Location = new System.Drawing.Point(12, 83);
+            this.timeTextBox.Name = "timeTextBox";
+            this.timeTextBox.Size = new System.Drawing.Size(100, 22);
+            this.timeTextBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(118, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "分";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // countDownFlowLayoutPanel
+            // 
+            this.countDownFlowLayoutPanel.Controls.Add(this.minLabel);
+            this.countDownFlowLayoutPanel.Controls.Add(this.label2);
+            this.countDownFlowLayoutPanel.Controls.Add(this.secLabel);
+            this.countDownFlowLayoutPanel.Location = new System.Drawing.Point(12, 140);
+            this.countDownFlowLayoutPanel.Name = "countDownFlowLayoutPanel";
+            this.countDownFlowLayoutPanel.Size = new System.Drawing.Size(200, 100);
+            this.countDownFlowLayoutPanel.TabIndex = 5;
+            // 
+            // minLabel
+            // 
+            this.minLabel.AutoSize = true;
+            this.minLabel.Font = new System.Drawing.Font("游ゴシック", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.minLabel.Location = new System.Drawing.Point(3, 0);
+            this.minLabel.Name = "minLabel";
+            this.minLabel.Size = new System.Drawing.Size(55, 43);
+            this.minLabel.TabIndex = 6;
+            this.minLabel.Text = "00";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("游ゴシック", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(64, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 43);
+            this.label2.TabIndex = 8;
+            this.label2.Text = ":";
+            // 
+            // secLabel
+            // 
+            this.secLabel.AutoSize = true;
+            this.secLabel.Font = new System.Drawing.Font("游ゴシック", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.secLabel.Location = new System.Drawing.Point(98, 0);
+            this.secLabel.Name = "secLabel";
+            this.secLabel.Size = new System.Drawing.Size(55, 43);
+            this.secLabel.TabIndex = 7;
+            this.secLabel.Text = "00";
+            // 
+            // CountDownTimer
+            // 
+            this.CountDownTimer.Interval = 1000;
+            this.CountDownTimer.Tick += new System.EventHandler(this.CountDownTimer_Tick);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.countDownCheckBox);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(218, 140);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(200, 100);
+            this.flowLayoutPanel1.TabIndex = 7;
+            // 
+            // countDownCheckBox
+            // 
+            this.countDownCheckBox.AutoSize = true;
+            this.countDownCheckBox.Location = new System.Drawing.Point(3, 3);
+            this.countDownCheckBox.Name = "countDownCheckBox";
+            this.countDownCheckBox.Size = new System.Drawing.Size(171, 34);
+            this.countDownCheckBox.TabIndex = 8;
+            this.countDownCheckBox.Text = "保存までの\r\nカウントダウンを表示する";
+            this.countDownCheckBox.UseVisualStyleBackColor = true;
+            this.countDownCheckBox.CheckedChanged += new System.EventHandler(this.countDownCheckBox_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 246);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Info";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(537, 280);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.countDownFlowLayoutPanel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.timeTextBox);
+            this.Controls.Add(this.onOffButton);
+            this.Controls.Add(this.processComboBox);
+            this.Controls.Add(this.reloadButton);
+            this.Name = "Form1";
+            this.Text = "自動でCtrl+S押す奴";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.countDownFlowLayoutPanel.ResumeLayout(false);
+            this.countDownFlowLayoutPanel.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button reloadButton;
+        private System.Windows.Forms.ComboBox processComboBox;
+        private System.Windows.Forms.Button onOffButton;
+        private System.Windows.Forms.TextBox timeTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.FlowLayoutPanel countDownFlowLayoutPanel;
+        private System.Windows.Forms.Label minLabel;
+        private System.Windows.Forms.Label secLabel;
+        private System.Windows.Forms.Timer CountDownTimer;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.CheckBox countDownCheckBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ AutoSaveMan/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoSaveMan
+{
+    public partial class Form1 : Form
+    {
+        List<System.Diagnostics.Process> processes = new List<System.Diagnostics.Process>();
+        bool start = false;
+        int interval = 1000;
+
+        public Form1()
+        {
+            InitializeComponent();
+            reloadButton_Click(this, null);
+        }
+
+        private void reloadButton_Click(object sender, EventArgs e)
+        {
+            processComboBox.Items.Clear();
+            processes = System.Diagnostics.Process.GetProcesses().OrderBy(p => p.MainWindowTitle).ToList();
+            processes.RemoveAll(p => p.MainWindowTitle == "");
+            foreach (var p in processes)
+            {
+                processComboBox.Items.Add(p.MainWindowTitle);
+            }
+        }
+
+        [DllImport("user32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        private void onOffButton_Click(object sender, EventArgs e)
+        {
+            if (!start)
+            {
+                // Check
+                if(timeTextBox.Text == "") { MessageBox.Show("入力して下さい"); return; }
+                try
+                {
+                    interval = int.Parse(timeTextBox.Text);
+                    if(interval < 5)
+                    {
+                        MessageBox.Show("5分以上にしてください");
+                        return;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("入力内容が正しくありません。\n" + ex.Message);
+                    return;
+                }
+                // Enable
+                onOffButton.Text = "停止";
+                processComboBox.Enabled = false;
+                timeTextBox.Enabled = false;
+                // Timer Start
+                timer1.Interval = interval * 60 * 1000;
+                timer1.Start();
+                // CountDown
+                minLabel.Text = interval.ToString().PadLeft(2,'0'); secLabel.Text = "00";
+                CountDownTimer.Start();
+
+                start = true;
+            }
+            else
+            {
+                onOffButton.Text = "開始";
+                timer1.Stop();
+                CountDownTimer.Stop();
+                start = false;
+                timeTextBox.Enabled = true;
+                processComboBox.Enabled = true;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int i = processComboBox.SelectedIndex;
+            var p = processes[i];
+            // Active
+            SetForegroundWindow(p.MainWindowHandle);
+            // Send Key
+            SendKeys.Send("^s");
+        }
+
+        private void CountDownTimer_Tick(object sender, EventArgs e)
+        {
+            int min = int.Parse(minLabel.Text);
+            int sec = int.Parse(secLabel.Text);
+            sec -= 1;
+            if (sec < 0)
+            {
+                sec = 59;
+                min -= 1;
+                if (min < 0)
+                {
+                    min = interval - 1;
+                }
+            }
+            minLabel.Text = min.ToString().PadLeft(2, '0');
+            secLabel.Text = sec.ToString().PadLeft(2, '0');
+        }
+
+        private void countDownCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (countDownCheckBox.Checked)
+            {
+                countDownFlowLayoutPanel.Visible = true;
+            }
+            else
+            {
+                countDownFlowLayoutPanel.Visible = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("製作者：@Shiso_no_ha\nこのソフトウェアによるいかなる損害に対しても製作者は責任を負いかねます。\n使用には自己責任でお願いします。");
+        }
+    }
+}
